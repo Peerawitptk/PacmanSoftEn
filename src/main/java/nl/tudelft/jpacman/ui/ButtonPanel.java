b18d9ef90bd22185ffebe4781dbff06d104a3790
peerawitptk <peerawit.p@kkumail.com>
2023-03-21 21:58:02 +0700

    Coding for UI menu in game

--- src/main/java/nl/tudelft/jpacman/ui/ButtonPanel.java
+++ src/main/java/nl/tudelft/jpacman/ui/ButtonPanel.java
@@ -1,7 +1,9 @@
 package nl.tudelft.jpacman.ui;
 
+import java.awt.Color;
 import java.util.Map;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -9,7 +11,7 @@ import javax.swing.JPanel;
 /**
  * A panel containing a button for every registered action.
  *
- * @author Jeroen Roosen 
+ * @author Jeroen Roosen
  */
 class ButtonPanel extends JPanel {
 
@@ -20,21 +22,76 @@ class ButtonPanel extends JPanel {
 
     /**
      * Create a new button panel with a button for every action.
+     * 
      * @param buttons The map of caption - action for each button.
-     * @param parent The parent frame, used to return window focus.
+     * @param parent  The parent frame, used to return window focus.
      */
+    ThemeSelect config = new ThemeSelect(); /* ADD NEW */
+
     ButtonPanel(final Map<String, Action> buttons, final JFrame parent) {
         super();
         assert buttons != null;
         assert parent != null;
+        if (config.getConfig() == "0") {
+            setBackground(Color.white);
+            for (final String caption : buttons.keySet()) {
+                ImageIcon picture = new ImageIcon(
+                        getClass().getResource("/sprite/" + caption.toLowerCase() + "_button.png"));
+                JButton button = new JButton(picture);
+                button.addActionListener(e -> {
+                    buttons.get(caption).doAction();
+                    parent.requestFocusInWindow();
+                });
+                button.setBorderPainted(false);
+                button.setContentAreaFilled(false);
+                add(button);
+            }
+        }
 
-        for (final String caption : buttons.keySet()) {
-            JButton button = new JButton(caption);
-            button.addActionListener(e -> {
-                buttons.get(caption).doAction();
-                parent.requestFocusInWindow();
-            });
-            add(button);
+        else if (config.getConfig() == "1") {
+            setBackground(Color.white);
+            for (final String caption : buttons.keySet()) {
+                ImageIcon picture = new ImageIcon(
+                        getClass().getResource("/sprite/" + caption.toLowerCase() + "_1button.png"));
+                JButton button = new JButton(picture);
+                button.addActionListener(e -> {
+                    buttons.get(caption).doAction();
+                    parent.requestFocusInWindow();
+                });
+                button.setBorderPainted(false);
+                button.setContentAreaFilled(false);
+                add(button);
+            }
+        }
+
+        else if (config.getConfig() == "2") {
+            setBackground(Color.white);
+            for (final String caption : buttons.keySet()) {
+                ImageIcon picture = new ImageIcon(
+                        getClass().getResource("/sprite/" + caption.toLowerCase() + "_1button.png"));
+                JButton button = new JButton(picture);
+                button.addActionListener(e -> {
+                    buttons.get(caption).doAction();
+                    parent.requestFocusInWindow();
+                });
+                button.setBorderPainted(false);
+                button.setContentAreaFilled(false);
+                add(button);
+            }
+        } else if (config.getConfig() == "3") {
+            setBackground(Color.white);
+            for (final String caption : buttons.keySet()) {
+                ImageIcon picture = new ImageIcon(
+                        getClass().getResource("/sprite/" + caption.toLowerCase() + "_2button.png"));
+                JButton button = new JButton(picture);
+                button.addActionListener(e -> {
+                    buttons.get(caption).doAction();
+                    parent.requestFocusInWindow();
+                });
+                button.setBorderPainted(false);
+                button.setContentAreaFilled(false);
+                add(button);
+            }
         }
     }
 }
